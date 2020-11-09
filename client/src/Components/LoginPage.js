70160da03349eacae2e5385b652045e8beb8bda0
Barrett Pope <barrettpope94@gmail.com>
2020-11-09 10:40:44 -0500

    Preparing for pull

--- client/src/Components/LoginPage.js
+++ client/src/Components/LoginPage.js
@@ -137,7 +137,7 @@ export default function SignInSide() {
               </Grid>
               <Grid item>
                 <Link href='#' variant='body2'>
-                  {"Don't have an account? Sign Up"}
+                  Create Account
                 </Link>
               </Grid>
             </Grid>
