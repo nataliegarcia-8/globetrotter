b1b5163e9779234c0121ed64fe44ef1338b33b74
Santiago Solana <santiago.sjs@gmail.com>
2020-12-07 20:36:27 -0500

    fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Stateside",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/globetrotter",
 // { useNewUrlParser: true },
 // { useUnifiedTopology: true }
 {
